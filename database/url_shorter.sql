bcbd298da3925b4d4485359c72728ae7f03d0f33
ZackyXOXO <seven.sixerz@gmail.com>
2024-02-19 11:43:51 +0700

    up

---
+++ database/url_shorter.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 05:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `url_shorter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url`
+--
+
+CREATE TABLE `url` (
+  `id` int(11) NOT NULL,
+  `fullUrl` varchar(255) NOT NULL,
+  `shortUrl` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `url`
+--
+
+INSERT INTO `url` (`id`, `fullUrl`, `shortUrl`, `count`) VALUES
+(1, 'https://www.geeksforgeeks.org/folder-structure-for-a-node-js-project/', 'RvFZ2yz8d', 1),
+(2, 'https://google.com', '5GY9OEac7', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `url`
+--
+ALTER TABLE `url`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `url`
+--
+ALTER TABLE `url`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
